8694f68093d3cd3f1620f190528b5d379a67c985
wen81643956 <619405464@qq.com>
2018-02-26 18:01:58 +0800

    电影页面以及组件开发

--- components/finder/index.js
+++ components/finder/index.js
@@ -4,15 +4,14 @@ Component({
       type: String,
       value: ''
     },
-    cardList: {
+    finderList: {
       type: Array,
       value: []
     }
   },
   data: {
     // 这里是一些组件内部数据
-    someData: {},
-    imageUrl: 'https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2514175916.jpg'
+    someData: {}
   },
   methods: {
     // 这里是一个自定义方法
